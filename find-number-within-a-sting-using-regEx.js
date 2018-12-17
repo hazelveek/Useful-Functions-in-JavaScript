b708612e2bf5758f69c0a32bf52338164c810fb8
Victoria Etim <victoria@initsng.com>
2018-12-17 11:38:57 +0100

    changed parameter namr

--- find-number-within-a-sting-using-regEx.js
+++ find-number-within-a-sting-using-regEx.js
@@ -1,16 +1,17 @@
 /**
- * Find exact digit number within a string
+ * Returns first occurence of exact digit number within a string
  * @param {number} digits 
- * @param {string} string 
+ * @param {string} stringToSearch 
  * @param {function} callback
  */
-function getExactDigitNumberWithinAString(digits, string, callback){
+function getExactDigitNumberWithinAString(digits, stringToSearch, callback){
     //returns build up the Regular expression
+    stringToSearch = stringToSearch.replace(/\"/g,'\\"');
     let RegXP = new RegExp('\\D\(\\d{'+digits+'}\)\\D');
 
     //return matching expression of false if none
-    if(RegXP.test(string)){
-        let matches = string.match(RegXP);
+    if(RegXP.test(stringToSearch)){
+        let matches = stringToSearch.match(RegXP);
         return callback(matches[1]);
     }
     return callback(false);
@@ -19,15 +20,16 @@ function getExactDigitNumberWithinAString(digits, string, callback){
 /**
  * Returns all occurence of exact digit numbers within a string
  * @param {number} digits 
- * @param {string} string 
+ * @param {string} stringToSearch 
  * @param {function} callback
  */
-function findExactDigitNumbersWithinAString(digits, string, callback){
+function findExactDigitNumbersWithinAString(digits, stringToSearch, callback){
+    stringToSearch = stringToSearch.replace(/\"/g,'\\"');
     let RegXP = new RegExp('\\D\(\\d{'+digits+'}\)\\D', 'g');
 
     //return matching expression of false if none
-    if(RegXP.test(string)){
-        let matches = string.match(RegXP);
+    if(RegXP.test(stringToSearch)){
+        let matches = stringToSearch.match(RegXP);
         let cleaned_matches = matches.map(function(match){
             let regx = new RegExp('\(\\d{'+digits+'}\)');
             return match.match(regx)[1];
