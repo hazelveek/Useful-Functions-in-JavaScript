57e6331210e85c483866846b6495020447cad743
Victoria Etim <victoria@initsng.com>
2019-04-22 00:04:01 +0100

    Added gitignore Modifies firebaseSnapshotToArray to work properly with childNodes

--- easily-manipulate-firebase-snapshot.js
+++ easily-manipulate-firebase-snapshot.js
@@ -1,30 +1,30 @@
-
 /**
  * Convert Firebase Snapshot To Array in Javascript
  * 
  * @param {FirebaseDataSnapshot} snapshot
  * @return {Array} val() Object with two method val() which is the array of data in the snapshot
  * @return {number} numChldren() retuns the number of elements in the val() array
+ * @author Victoria Etim
  */
 function firebaseSnapshotToArray(snapshot, ischild = false){
   let numChildren = snapshot.numChildren();
   let arraySnap = Array();
-  for(childSnap of snapshot){
-    let data = childSnap.val();
-    data.key = childSnap.key();
-    if(typeof data === 'object'){
-      data = firebaseSnapshotToArray(childSnap).val();
-    }
-    arraySnap.push(data);
-  }
+  snapshot.forEach(childSnap => {
+      let data = childSnap.val();
+      data.key = childSnap.key;
+      if(data.numChildren){
+          data = firebaseSnapshotToArray(childSnap, true);
+      }
+      arraySnap.push(data);
+  })
   if(ischild) return arraySnap;
 
   return {
-    val: () => {
-      return arraySnap
-    },
-    numChildren: () => {
-      return numChildren;
-    }
+      val: () => {
+          return arraySnap
+      },
+      numChildren: () => {
+          return numChildren;
+      }
   }
-}  
+}
